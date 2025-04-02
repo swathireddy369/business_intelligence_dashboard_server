8691b2df1bc01f71becf29a5bd9fa0cedb1d0c62
swathireddy369 <swathiamaravadi53@gmail.com>
2025-04-02 14:59:09 -0400

    initial commit

---
+++ src/main/java/com/example/business_intelligence_dashboard_server/BusinessIntelligenceDashboardServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.business_intelligence_dashboard_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessIntelligenceDashboardServerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hiii");
+		SpringApplication.run(BusinessIntelligenceDashboardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/business_intelligence_dashboard_server/BusinessIntelligenceDashboardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.business_intelligence_dashboard_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessIntelligenceDashboardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
